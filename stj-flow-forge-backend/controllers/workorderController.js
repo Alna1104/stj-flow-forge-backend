3c6760bb9fc22a065d01f1aab18af259780b07a6
Alna1104 <alnabchungath@gmail.com>
2025-06-21 15:54:16 +0530

    Update workorderController.js

---
+++ stj-flow-forge-backend/controllers/workorderController.js
@@ -0,0 +1,12 @@
+const createWorkOrder = async (req, res) => {
+  // ...
+};
+
+const getWorkOrders = async (req, res) => {
+  // ...
+};
+
+module.exports = {
+  createWorkOrder,
+  getWorkOrders,
+};
