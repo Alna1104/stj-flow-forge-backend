58b88c67d5f89a30282d60fb74e357ded60a1628
Alna1104 <alnabchungath@gmail.com>
2025-06-21 17:29:51 +0530

    Update workorders.js

--- stj-flow-forge-backend/routes/workorders.js
+++ stj-flow-forge-backend/routes/workorders.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createWorkOrder, getWorkOrders } = require('../controllers/workorderController');
 
-router.post('/', createWorkOrder);
-router.get('/', getWorkOrders);
+router.post("/", createWorkOrder);
+router.get("/", getWorkOrders);
 
 module.exports = router;
