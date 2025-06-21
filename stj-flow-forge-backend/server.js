8e6ef9b3c1c628bcf7f771ae56513559bc73f1c8
Alna1104 <alnabchungath@gmail.com>
2025-06-21 16:52:23 +0530

    Update server.js

--- stj-flow-forge-backend/server.js
+++ stj-flow-forge-backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
