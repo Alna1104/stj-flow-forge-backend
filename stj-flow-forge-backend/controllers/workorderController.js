e9371b30f38092ac4940414234a28d9ac1651e97
Alna1104 <alnabchungath@gmail.com>
2025-06-21 15:57:28 +0530

    Update workorderController.js

--- stj-flow-forge-backend/controllers/workorderController.js
+++ stj-flow-forge-backend/controllers/workorderController.js
@@ -1,12 +1,22 @@
+const WorkOrder = require('../models/WorkOrder');
+
 const createWorkOrder = async (req, res) => {
-  // ...
+  try {
+    const workOrder = new WorkOrder(req.body);
+    await workOrder.save();
+    res.status(201).json(workOrder);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const getWorkOrders = async (req, res) => {
-  // ...
+  try {
+    const orders = await WorkOrder.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
-module.exports = {
-  createWorkOrder,
-  getWorkOrders,
-};
+module.exports = { createWorkOrder, getWorkOrders };
