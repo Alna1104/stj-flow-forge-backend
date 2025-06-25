3cd303ec186d5ad1218dc8354f21f790f2fd1b16
Alna1104 <alnabchungath@gmail.com>
2025-06-25 14:01:44 +0530

    Update Company.js

--- models/Company.js
+++ models/Company.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
-const companySchema = new mongoose.Schema({
-  companyName: { type: String, required: true },
+const contactSchema = new mongoose.Schema({
   contactPerson: { type: String, required: true },
   phone: String,
   email: String,
-  department: String,
+  department: String
+});
+
+const companySchema = new mongoose.Schema({
+  companyName: { type: String, required: true },
+  contacts: [contactSchema],
   createdAt: { type: Date, default: Date.now }
 });
 
